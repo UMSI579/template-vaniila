f499f4b95e3cdb7411cdb4c010b1233f9f16d751
bnjmnm <benm@umich.edu>
2022-08-11 07:41:16 -0400

    there are no component tests for this tempalte

--- cypress/e2e/e2eTest.cy.js
+++ cypress/e2e/e2eTest.cy.js
@@ -1,4 +1,4 @@
-describe('Midterm1', () => {
+describe('Test the template', () => {
   beforeEach(() => {
     // Cypress starts out with a blank slate for each test
     // so we must tell it to visit our website with the `cy.visit()` command.
